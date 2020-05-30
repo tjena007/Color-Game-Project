61394e2c91ffeb763fb7e86a0ba1d98eaa5ae6fc
Tanmay Jena <tjena2@gmail.com>
2020-05-30 16:02:51 +0530

    Completed logic, added some extra functionality

--- color.js
+++ color.js
@@ -1,9 +1,11 @@
 var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-
+var newcolor = document.querySelector("#newcolor");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colordisplay");
+
+var h1 = document.querySelector("h1");
 colorDisplay.textContent = pickedColor;
 
 var message = document.querySelector("#message");
@@ -23,6 +25,7 @@ for (let index = 0; index < squares.length; index++) {
     else {
       message.textContent = "Correct";
       changeColor();
+      newcolor.textContent = "Play Again?";
     }
   });
 }
@@ -31,14 +34,44 @@ function changeColor() {
   for (let index = 0; index < squares.length; index++) {
     squares[index].style.backgroundColor = pickedColor;
   }
+  h1.style.backgroundColor = pickedColor;
 }
 
 function pickColor() {
-  //Math.floor(Math.random() * 256);
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
 }
 
 function generateRandomColors(num) {
   var arr = [];
+
+  for (let index = 0; index < num; index++) {
+    arr.push(randomColors());
+  }
+  return arr;
 }
+
+function randomColors() {
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+
+  var a = "rgb(" + red + ", " + green + ", " + blue + ")";
+  return a;
+}
+
+newcolor.addEventListener("click", function () {
+  newcolor.textContent = "New Colours";
+  //generate colors
+  colors = generateRandomColors(6);
+  //assign new picked color
+  pickedColor = pickColor();
+  //chnage header
+  colorDisplay.textContent = pickedColor;
+  //generate colors
+  for (let index = 0; index < squares.length; index++) {
+    squares[index].style.backgroundColor = colors[index];
+  }
+  //reset header background
+  h1.style.background = "#232323";
+});
